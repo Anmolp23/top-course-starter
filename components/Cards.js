70b05f7d9865af5c83af81b250d158be047470e4
Anmol parihar <121367744+Anmolp23@users.noreply.github.com>
2024-01-04 17:14:37 +0530

    Create Cards.js

---
+++ components/Cards.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Card from "./Card";
+
+export default function Cards(props) {
+  let courses = props.courses;
+  let category = props.courses;
+  const [likedCourses, setLikedCourses] = useState([]);
+
+  const getCourses = () => {
+    if (category === "All") {
+      let allCourses = [];
+      Object.values(courses).forEach((array) => {
+        array.forEach((courseData) => {
+          allCourses.push(courseData);
+        });
+      });
+      return allCourses;
+    } else {
+      return courses[category];
+    }
+  };
+
+  return (
+    <div>
+      {getCourses().map((course) => (
+        <Card
+          key={course.id}
+          course={course}
+          likedCourses={likedCourses}
+          setLikedCourses={setLikedCourses}
+        />
+      ))}
+    </div>
+  );
+}
